eb348c5fb9ef4fbca378b4e78e5b3b8dfe2165bf
Neo972 <jameswest972@yahoo.fr>
2019-05-09 11:57:38 +0200

    premiere sauvegarde

---
+++ src/pages/Todo/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const URL = 'https://jsonplaceholder.typicode.com/todos'
+function Todo() {
+
+    return (
+        <div>
+            <h1>Liste des taches </h1>
+
+        </div>
+    );
+}
+export default Todo;
